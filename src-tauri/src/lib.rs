487932ba0e001259f4a17b0c30459379f5a39840
誠 遺憾 <ikasan2860@gmail.com>
2026-06-02 17:29:26 +0900

    reverted all pull requests

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-#![allow(linker_messages)]
-
-mod app_config;
-mod commands;
-mod discord_rpc;
-mod errors;
-mod riot;
-mod henrik;
-mod http_log;
-mod models;
-mod state;
-
-use app_config::ensure_config_dir;
-use commands::discord::{discord_rpc_clear, discord_rpc_set_activity};
-use commands::henrik::{
-    henrik_account_by_name, henrik_account_by_puuid, henrik_content, henrik_crosshair,
-    henrik_esports_schedule, henrik_get_settings, henrik_leaderboard, henrik_match_by_id,
-    henrik_matches_by_name, henrik_matches_by_puuid, henrik_mmr_by_name, henrik_mmr_by_puuid,
-    henrik_mmr_history_by_name, henrik_mmr_history_by_puuid, henrik_save_api_key,
-    henrik_vlr_event_matches, henrik_vlr_events, henrik_vlr_match, henrik_vlr_player,
-    henrik_vlr_player_matches, henrik_vlr_team, henrik_vlr_team_matches,
-    henrik_vlr_team_transactions,
-};
-use log::LevelFilter;
-use state::AppState;
-use tauri_plugin_log::{Target, TargetKind};
-=======
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 mod riot;
 use riot::ValorantAPI;
@@ -184,7 +156,6 @@ async fn get_region() -> Result<String, String> {
 
     api.get_region().await
 }
->>>>>>> 3ea16ef1c38eb97cc19d82291190497bca6d9c9e
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -200,48 +171,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-<<<<<<< HEAD
-            commands::riot_command::get_presence,
-            commands::riot_command::get_all_presences,
-            commands::riot_command::get_my_presence,
-            commands::riot_command::get_auth_userinfo,
-            commands::riot_command::get_private_presence,
-            commands::riot_command::get_gamestate,
-            commands::riot_command::is_api_initialized,
-            commands::riot_command::get_puuid,
-            commands::riot_command::get_full_username,
-            commands::riot_command::get_playercard_by_id,
-            commands::riot_command::get_region,
-            commands::riot_command::get_player_mmr,
-            commands::riot_command::get_player_loadout,
-            commands::riot_command::get_storefront,
-            commands::riot_command::get_current_match,
-            henrik_get_settings,
-            henrik_save_api_key,
-            henrik_account_by_name,
-            henrik_account_by_puuid,
-            henrik_content,
-            henrik_crosshair,
-            henrik_esports_schedule,
-            henrik_leaderboard,
-            henrik_matches_by_name,
-            henrik_matches_by_puuid,
-            henrik_match_by_id,
-            henrik_mmr_by_name,
-            henrik_mmr_by_puuid,
-            henrik_mmr_history_by_name,
-            henrik_mmr_history_by_puuid,
-            henrik_vlr_events,
-            henrik_vlr_event_matches,
-            henrik_vlr_match,
-            henrik_vlr_team,
-            henrik_vlr_team_matches,
-            henrik_vlr_team_transactions,
-            henrik_vlr_player,
-            henrik_vlr_player_matches,
-            discord_rpc_set_activity,
-            discord_rpc_clear
-=======
             greet,
             get_presence,
             get_my_presence,
@@ -254,7 +183,6 @@ pub fn run() {
             get_playercard_by_id,
             get_region,
             get_player_mmr
->>>>>>> 3ea16ef1c38eb97cc19d82291190497bca6d9c9e
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
